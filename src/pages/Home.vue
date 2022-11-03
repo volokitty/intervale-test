1f4d8cf4b26e1147101ee63c4e5f3000b1073c85
volokitty <v.volokitty@gmail.com>
2022-11-03 14:53:03 +0300

    feat: create vue app

---
+++ src/pages/Home.vue
@@ -0,0 +1,11 @@
+<template>
+  <div>Home</div>
+</template>
+
+<script lang="ts">
+export default {
+  name: "HomePage",
+};
+</script>
+
+<style></style>
---
+++ src/router/index.ts
@@ -0,0 +1,16 @@
+import { createRouter, createWebHistory, RouteRecordRaw } from "vue-router";
+import Home from "@/pages/Home.vue";
+
+const routes: Array<RouteRecordRaw> = [
+  {
+    path: "/",
+    component: Home,
+  },
+];
+
+const router = createRouter({
+  history: createWebHistory(process.env.BASE_URL),
+  routes,
+});
+
+export default router;
